10f801efade2e317f53fcd0f9a30ddae83b77c42
ippei tada <ipp.tada@gmail.com>
2026-04-20 00:06:00 +0900

    Refine terminal appearance and fonts

---
+++ lua/wezterm_config/theme.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+M.colors = {
+  bg = '#262624',
+  bg_dark = '#1f1f1d',
+  bg_highlight = '#30302c',
+  bg_visual = '#3a3934',
+  selection = '#3a4a55',
+  fg = '#dcd7ba',
+  fg_dim = '#c8c093',
+  muted = '#727169',
+  blue = '#7e9cd8',
+  cyan = '#7aa89f',
+  green = '#98bb6c',
+  orange = '#ffa066',
+  red = '#e46876',
+  yellow = '#e6c384',
+}
+
+return M
